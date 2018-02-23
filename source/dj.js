710bb6f21b9f112384a71ddfd7d6f610b920ab76
Dhrusya <gopal@dhrusya.com>
2018-02-24 03:58:28 +0530

    Major update with query selector

--- source/dj.js
+++ source/dj.js
@@ -5,54 +5,63 @@ var myQuery, dj, djob;
 (function() {
 
      dj = function(selector) {
-		var djob;
-		if(selector.substr(0,1)=="#"){ // if id
-			djob=document.getElementById(selector.substr(1));
-		}else if(selector.substr(0,1)=="."){ //if class, slow in ie
-			var djob=[];
-			if(document.getElementsByClassName){
-				var nodes=document.getElementsByClassName(selector.substr(1));
-				for (var i = 0; i < nodes.length; i++) djob[i] = nodes[i];
-			}else{ //if given tagname
-				var nodes = document.getElementsByTagName("*");
-				for(z=0;z<nodes.length;z++){
-					if(nodes[z].getAttribute("class") == selector.substr(1))djob.push(nodes[z]);
-				}
-			}
-		}else{
-			var nodes = document.getElementsByTagName(selector);
-			var djob=new Array();
-			for (var i = 0; i < nodes.length; i++) djob[i] = nodes[i];
-		}
-		if (!is_array(djob)) djob = [djob];
-        return new djs(djob);
+      var djob=this;
+  		if(typeof(selector)=='object'){
+        djob = [selector];
+      }else{
+        djob = document.querySelectorAll(selector);
+      }
+      return new djs(djob);
     };
-	
+
 	var djs=function(ob){
 		return {
 			remove:function(){ //removes element
-				doExec(ob,function(aob){ aob.parentNode.removeChild(aob)});
+				doExec(ob,function(aob){ aob.parentNode.removeChild(aob);});
 			},
 			empty:function(){ //makes element empty
-				doExec(ob,function(aob){ aob.innerHTML=""});
+				doExec(ob,function(aob){ aob.innerHTML="";});
 			},
 			html:function(str){ //gets or sets innerHTML
-				if(str)	doExec(ob,function(aob){ aob.innerHTML=str});
-				else return doExec(ob,function(aob){ return aob.innerHTML},true);
+				if(str)	doExec(ob,function(aob){ aob.innerHTML=str;});
+				else return doExec(ob,function(aob){ return aob.innerHTML;},true);
 			},
 			hide:function(){
-				doExec(ob,function(aob){ aob.style.display="none"});	
+				doExec(ob,function(aob){ aob.style.display="none";});
 			},
 			show:function(){
-				doExec(ob,function(aob){ aob.style.display="block"});	
+				doExec(ob,function(aob){ aob.style.display="block";});
 			},
-		}
-	}
+      on:function(type,callback){
+        doExec(ob,function(aob){ aob['on'+type]=callback;});
+      },
+      toString:function(item){
+        return this.selector;
+      },
+      each:function(callback){
+        // doExec(ob, action);
+        ob.forEach.call(ob, function(item, i){
+            console.log(i + item);
+            callback(item, i);
+            // doExec(item, action);
+        });
+      },
+      // model:function(){
+      //   alert("Bingo");
+      // },
+		};
+	};
 
-	
+
 
 
 }());
+
+// dj.sample = function() {
+//         alert("Nee tata parri");
+//         //do something here
+//         //return that; //if you want to chain.
+// };
 
 	function doExec(ob, callback,returnFlag) {
 		var ret="";
@@ -61,56 +70,56 @@ var myQuery, dj, djob;
 			else callback(ob[i]);
 		}
 		return ret;
-	};
+	}
 
-    
+
 	function is_array(input){
     	return typeof(input)=='object'&&(input instanceof Array);
- 	};
-	
+ 	}
+[]
 
 
-//AJAX	
+//AJAX
 var ajax=function(){
-	
+
 	//REQ
 	this.loadReq=function(){
 		var req = (window.XMLHttpRequest) ? new XMLHttpRequest() : ((window.ActiveXObject) ? new ActiveXObject("Microsoft.XMLHTTP") : null);
-		if (req == null) {
+		if (req === null) {
 			alert("Seems this browser not supportd AJAX");
 			return false;
 		}else {
-			return req;	
-		}		
-	}
-	
+			return req;
+		}
+	};
+
 	this.arrayToURL=function(params, method) {
 		var urlStr = method == 'GET' ? '?' : '';
 		for (var param in params) urlStr += param + '=' + params[param] + '&';
 		return urlStr;
-	}
-			
+	};
+
 	this.getData=function(url, return_func_or_id, method, params,callback) {
 		if(!callback) callback=false;
 		if(!method) method="GET";
 		if(!params) params="";
-		
+
 		var req=this.loadReq();
-		if(req){ 
-			var parameters = params != null ? this.arrayToURL(params, method) : null;
+		if(req){
+			var parameters = params !== null ? this.arrayToURL(params, method) : null;
 			method=!method?"GET":method;
 			var URL = method == 'GET' ? url + parameters : url;
-			
-			req.open(method, URL, true)
-			
+
+			req.open(method, URL, true);
+
 			req.setRequestHeader('User-Agent','XMLHTTP/1.0');
 			if (method == 'POST') req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-			
+
 			req.onreadystatechange = function() {
 				//o=document.getElementById(id);
 				if (req.readyState == 4 && req.status == 200) {
-					if(callback==true){
-						funcl=return_func_or_id.length-1
+					if(callback===true){
+						funcl=return_func_or_id.length-1;
 						func=return_func_or_id.substr(0,(funcl));
 						//alert(func)
 						eval(func+ 'req.responseText)');
@@ -118,12 +127,12 @@ var ajax=function(){
 						o=document.getElementById(return_func_or_id);
 						o.innerHTML=req.responseText;
 					}
-					delete req;
+					// delete req;
 				}
-			}
+			};
 			req.send(parameters);
 		}
-	}
+	};
 	//END
 
 };
@@ -145,19 +154,19 @@ function dj_buildForm(json){
 	fields+="</form>";
 	document.write(fields);
 	//alert(json.method);
-};
+}
 
 function dj_prepareFields(flds){
 	var data="";
-	for(f in flds){
+	for(var f in flds){
 		var ftype=flds[f].ftype;
-		
+
 		switch(ftype){
 			case 'input':
 				if(flds[f].values){
-					data+=dj_getFProps(flds[f],'<dd>', '</dd></dl>');	
+					data+=dj_getFProps(flds[f],'<dd>', '</dd></dl>');
 				}else{
-					data+=dj_getFProps(flds[f],'<dd><input ', '/></dd></dl>');	
+					data+=dj_getFProps(flds[f],'<dd><input ', '/></dd></dl>');
 				}
 				break;
 			case 'hidden':
@@ -168,41 +177,41 @@ function dj_prepareFields(flds){
 				break;
 			case "select":
 				data+=dj_prepareSelect(flds[f]);
-				
+
 				break;
 		}
 
 	}
-	
+
 	return data;
-};
+}
 
 function dj_getFProps(arr,begin,end){
 	var data="";
 	var label=(arr.label)?"<dl><dt>"+arr.label+"</dt>":'';
 	data+=label+begin;
 	if(arr.values){
-		for(v in arr.values){
+		for(var v in arr.values){
 			data+='<input ';
 			data+=dj_getProps(arr.props);
 			data+='value="'+v+'"';
 			data+='/>';
-			data+="<label>"+arr.values[v]+"</label>";	
+			data+="<label>"+arr.values[v]+"</label>";
 		}
 	}else{
 		data+=dj_getProps(arr.props);
 	}
 	data+=end;
 	return data;
-};
+}
 
 function dj_getProps(arr){
 	var data='';
-	for(p in arr){
+	for(var p in arr){
 		data+= p+"=\""+arr[p]+"\" ";
 	}
 	return data;
-};
+}
 
 function dj_prepareSelect(arr){
 	var data="";
@@ -212,20 +221,19 @@ function dj_prepareSelect(arr){
 	data+=">";
 	//put values
 	if(arr.values){
-		data+=dj_getOptions(arr.values);	
+		data+=dj_getOptions(arr.values);
 	}
 	data+="</select></dd></dl>";
 	return data;
-};
+}
 
 
 function dj_getOptions(arr,id){
 	if(!id) id=false;
 	data='';
-	for(v in arr){
+	for(var v in arr){
 		data+='<option value="'+v+'">'+arr[v]+'</option>';
 	}
 	if(id) document.getElementById(id).innerHTML=data;
 	return data;
-};
-
+}
--- source/sample.php
+++ source/sample.php
@@ -21,23 +21,41 @@
 
 <div id="id1" class="a">This is sample text</div>
 
-<div class="a">This is sample text</div>
+<div class="a pp1">This is sample text PP1</div>
 
 <div>This is sample text</div>
 
-<div class="a">This is sample text</div>
+<div class="a pp2">This is sample text PP2</div>
 
 Click here: <input type="text" class="color" value="">
 
 <input type="button" value="empty this" onclick="dj('#id1').empty()" />
+<input type="button" value="Load Id1" onclick="dj('#id1').html('Bla bla')" />
 
 <input type="button" value="Fill Text" onclick="dj('div.a').html('This is loaded by djs')" />
+<input type="button" value="Fill Css3 Text" onclick="vgTxt()" />
 
 <input type="button" value="hide this" onclick="dj('#id1').hide()" />
 
 <input type="button" value="show this" onclick="dj('#id1').show()" />
+<input type="button" value="Extension" onclick="vgEvent()" />
+
+<script type="text/javascript">
+  function vgTxt(){
+    dj('[class*="pp"]').html('This is loaded by djs queryselect all')
+  }
 
 
+    dj("#id1").on('click', function(e){
+      alert("click event")
+    });
+
+    dj("div.a").each(function(e){
+      //  e.innerHTML="Jamma Cekka";
+      dj(e).html("Laskutapa");
+    });
+
+</script>
 </body>
 
 </html>
